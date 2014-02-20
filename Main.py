ab20b50a7f8756be6279a3b5e0eb6cf0be40c80c
stowka <antoine@netproduction.fr>
2014-02-20 09:55:50 +0100

    first commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+#!/usr/python
+#-*- coding: utf-8 -*-
+
+
+targets = input("Number of targets:")
+print "---"
+sensors = input("Number of sensors:")
+print "---"
+for i in range(sensors):
---
+++ Sensor.py
@@ -0,0 +1,44 @@
+#!/usr/python
+#-*- coding: utf-8 -*-
+
+"""
+@project Technique d'optimisation
+@author Antoine De Gieter
+@author Alexis Beaujon
+"""
+class Sensor:
+	"""
+	Sensor:
+		x: position x
+		y: position y
+		radius: coverage radius
+		life: battery
+	"""
+
+	def __init__(self, x, y, radius, life):
+		print "New Sensor\n"
+		self._x = x
+		self._y = y
+		self._radius = radius
+		self._life = life
+		print self
+
+	def __str__(self):
+		return "[" + self._x + ", " + self._y + ", " + self._radius + "]"
+
+	"""
+	Getters
+	"""
+	def _get_x(self):
+		return self._x
+
+	def _get_y(self):
+		return self._y
+
+	def _get_radius(self):
+		return self._radius
+
+	def _get_life(self):
+		return self._life
+
+	def
